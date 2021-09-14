0dbf20789819b09ed1dc64e9738e6a1835d5b6a1
蘇子維 <kuanibuki0404@gmail.com>
2021-09-14 09:22:26 +0800

    import label

--- source/main.py
+++ source/main.py
@@ -152,7 +152,7 @@ def generate_real_samples(dataset, n_samples):
 	# retrieve selected images
 	X = dataset[ix]
 	# generate 'real' class labels (1)
-	y = ones((n_samples, 1))
+	y = measure['feature']
 	return X, y
 
 # generate n fake samples with class labels
@@ -167,5 +167,19 @@ def generate_fake_samples(n_samples):
 
 #training
 
+# train the discriminator model
+def train_discriminator(model, dataset, n_iter=100, n_batch=256):
+	half_batch = int(n_batch / 2)
+	# manually enumerate epochs
+	for i in range(n_iter):
+		# get randomly selected 'real' samples
+		X_real, y_real = generate_real_samples(dataset, half_batch)
+		# update discriminator on real samples
+		_, real_acc = model.train_on_batch(X_real, y_real)
+		# generate 'fake' examples
+		X_fake, y_fake = generate_fake_samples(half_batch)
+		# update discriminator on fake samples
+		_, fake_acc = model.train_on_batch(X_fake, y_fake)
+		# summarize performance
+		print('>%d real=%.0f%% fake=%.0f%%' % (i+1, real_acc*100, fake_acc*100))
 
-# 
