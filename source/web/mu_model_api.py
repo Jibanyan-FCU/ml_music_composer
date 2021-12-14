e50b018f811ffff8d7ac04fa7216dbf04cf8a2e8
Jibanyan-FCU <d0745587@mail.fcu.edu.tw>
2021-12-14 23:50:09 +0800

    delete old trained model

--- source/web/mu_model_api.py
+++
@@ -1,9 +0,0 @@
-import mu_model
-
-def get_new_music(*args, **kwargs):
-    model = mu_model.Mu_Model()
-    return model.make_music()
-
-# print(__name__)
-if __name__ == '__main__':
-    get_new_music()
