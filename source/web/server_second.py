019d49c0a9c0968da8bbbcb4290774193e5f2356
Jibanyan-FCU <d0745587@mail.fcu.edu.tw>
2021-12-16 20:13:43 +0800

    connected web and model

--- source/web/server_second.py
+++ source/web/server_second.py
@@ -1,23 +1,44 @@
-from flask import Flask, render_template, request
+from mu_model import model_api
 from database import db_api
 
+from flask import Flask, render_template, request, send_from_directory
+
+
 app = Flask(__name__)
+model_api.initial()
 
 COMPARE_PATH = '/static/music/compare'
+OUTPUT_PATH = '../mu_model/output'
+
 @app.route('/', methods=['GET', "POST"])
 def index():
 
     style = request.values.get('style')
+    generate = request.values.get('generate')
+    file = ""
+
     if style == None:
         style = ""
-    print(">>> ", style)
+
+    if generate == "true":
+        hidden = ""
+        file = model_api.make_music()
+        file = file.split('/')[-1]
+    else:
+        hidden = "hidden"
+
     db_api.open_db()
     df = db_api.search_compare_by_style(style)
     df = df.sample(5)
     df = df.sort_values("id")
 
     c_path = COMPARE_PATH
-    return render_template('result.html', dataframe=df, compare_storage=c_path)
+    return render_template('result.html', dataframe=df, compare_storage=c_path, hidden=hidden, download_file=file)
+
+@app.route("/download/<filename>")
+def download(filename):
+    print(app.root_path)
+    return send_from_directory(OUTPUT_PATH, filename, as_attachment=True)
 
 
 if __name__ == '__main__':
